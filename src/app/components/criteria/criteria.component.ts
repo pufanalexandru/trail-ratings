0ad40fbf0ea346bd69974ef0456cb191a52d6d32
Alexandru Pufan <pufanalexandru@gmail.com>
2017-01-15 13:44:01 +0200

    select thumbnails, add selected class

--- src/app/components/criteria/criteria.component.ts
+++ src/app/components/criteria/criteria.component.ts
@@ -8,11 +8,17 @@ import { DataService } from '../../services/data.service';
 export class CriteriaComponent implements OnInit {
 
   private widths: any[];
+  private selectedWidth: number;
 
   constructor(private data: DataService) { }
 
   ngOnInit() {
     this.widths = this.data.getWidths();
+  }
+
+  selectWidth(id) {
+    console.log(id);
+    this.selectedWidth = id;
   }
 
 }
