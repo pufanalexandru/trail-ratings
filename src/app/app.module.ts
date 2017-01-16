ffc5e379f01286328dd9623c540f9e38b032e7ad
Alexandru Pufan <pufanalexandru@gmail.com>
2017-01-16 18:59:52 +0200

    created tread-surface-options component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MeasurmentSystemService } from './services/measurment-system.service';
 import { DataService } from './services/data.service';
 import { TrailWidthOptionComponent } from './components/criteria/trail-width-option/trail-width-option.component';
 import { CenteredDirective } from './directives/centered.directive';
+import { TreadSurfaceOptionsComponent } from './components/criteria/tread-surface-options/tread-surface-options.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { CenteredDirective } from './directives/centered.directive';
     ResultComponent,
     UnitPipe,
     TrailWidthOptionComponent,
-    CenteredDirective
+    CenteredDirective,
+    TreadSurfaceOptionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/criteria/tread-surface-options/tread-surface-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tread-surface-options',
+  templateUrl: './tread-surface-options.component.html',
+  styleUrls: ['./tread-surface-options.component.css']
+})
+export class TreadSurfaceOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
